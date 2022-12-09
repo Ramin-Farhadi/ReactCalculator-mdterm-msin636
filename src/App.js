a608bba00c30fb0c20288afacb1a8204f2a9d065
Ramin <farhadiramin91@gmail.com>
2022-12-08 20:20:30 -0500

    Ramin Farhadi | MSIN 636 |  Kevin Roach

--- src/App.js
+++ src/App.js
@@ -1,59 +1,9 @@
-import { useState } from 'react';
-import './App.css';
 import './css/general.css';
+import Calculator from './Components/Calculator';
 function App() {
-  const [output, setOutPut] = useState('');
-  const calculator = (e) => {
-    if (output === '' && e.target.outerText === 'Del') {
-      setOutPut('');
-    }
-    if (output.length <= 1 && e.target.outerText === '0') {
-      setOutPut('');
-      return;
-    }
-    if (output.length > 0 && e.target.outerText === 'Del') {
-      setOutPut(output.slice(0, output.length - 1));
-    }
-
-    if (e.target.outerText === '=') {
-      setOutPut(eval(output));
-    } else if (e.target.outerText === 'AC') {
-      setOutPut('');
-    } else if (e.target.outerText !== 'Del') {
-      if (e.target.outerText === '.' && output[output.length - 1] === '.') {
-        setOutPut(output);
-      } else {
-        setOutPut(output + e.target.outerText);
-      }
-    }
-  };
   return (
     <div className="container">
-      <div className="output">{output}</div>
-      <div className="buttons">
-        <button onClick={(e) => calculator(e)} className="ac span-two">
-          AC
-        </button>
-        <button onClick={(e) => calculator(e)}>Del</button>
-        <button onClick={(e) => calculator(e)}>/</button>
-        <button onClick={(e) => calculator(e)}>7</button>
-        <button onClick={(e) => calculator(e)}>8</button>
-        <button onClick={(e) => calculator(e)}>9</button>
-        <button onClick={(e) => calculator(e)}>*</button>
-        <button onClick={(e) => calculator(e)}>4</button>
-        <button onClick={(e) => calculator(e)}>5</button>
-        <button onClick={(e) => calculator(e)}>6</button>
-        <button onClick={(e) => calculator(e)}>-</button>
-        <button onClick={(e) => calculator(e)}>1</button>
-        <button onClick={(e) => calculator(e)}>2</button>
-        <button onClick={(e) => calculator(e)}>3</button>
-        <button onClick={(e) => calculator(e)}>+</button>
-        <button onClick={(e) => calculator(e)}>0</button>
-        <button onClick={(e) => calculator(e)}>.</button>
-        <button onClick={(e) => calculator(e)} className="span-two">
-          =
-        </button>
-      </div>
+      <Calculator />
     </div>
   );
 }
